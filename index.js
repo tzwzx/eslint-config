f21626d029a8d24f3524b0e827029f3249d0edee
Naoto Tazawa <naoto.tazawa@gmail.com>
2023-12-22 22:27:18 +0900

    Add eslint configuration and setup
    This commit introduces eslint to the codebase. It adds a new package.json file that contains dependencies for eslint and related plugins. It also creates an index.js file as the main config, and adds a .gitignore file to ignore the node_modules directory.

---
+++ index.js
@@ -0,0 +1,56 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "node": true,
+    "es2022": true
+  },
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "project": "./tsconfig.json"
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:playwright/recommended",
+    "next/core-web-vitals",
+    "prettier"
+  ],
+  "ignorePatterns": ["next.config.js"],
+  "plugins": [
+    "@typescript-eslint",
+    "unused-imports",
+    "no-relative-import-paths",
+    "perfectionist"
+  ],
+  "rules": {
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      {
+        "prefer": "type-imports"
+      }
+    ],
+    "no-relative-import-paths/no-relative-import-paths": [
+      "error",
+      {
+        "allowSameFolder": true,
+        "rootDir": "src",
+        "prefix": "@"
+      }
+    ],
+    "perfectionist/sort-imports": "off",
+    "react/jsx-curly-brace-presence": "error",
+    "react/self-closing-comp": [
+      "error",
+      {
+        "component": true,
+        "html": false
+      }
+    ]
+  }
+}
